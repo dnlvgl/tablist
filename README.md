82ec52b09c5d42b97afda8577df7797e75de0e7f
Daniel Vogel <hello@dnlvgl.com>
2019-03-10 10:40:36 +0100

    Refactor js, add function to close tabs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Tablist
 
-Show all open tabs as a list of URLs.
+Show all open tabs in a popup. Select or export multiple tabs.
 
 At the moment all tabs of all windows are shown.
 
@@ -8,8 +8,10 @@ At the moment all tabs of all windows are shown.
 
 - [x] Add basic CSS for popup
 - [ ] Replace placeholder Icons (multiple sizes)
-- [ ] Add settings (copy all URLs or only focused window)
-- [ ] Add 'copy to clipboard' button?
+- [ ] Add 'export urls' button
+- [ ] Add tab favicon
+- [x] Add checkbox before tab name to select and close multiple tabs
+- [ ] Add search?
 
 ## Misc
 
--- popup/tablist.js
+++ popup/tablist.js
@@ -4,27 +4,104 @@ window.onload = function() {
 };
 */
 
-function logTabs(tabs) {
-    const popupList = document.getElementById('tablist-container');
-    let urlList = [];
+// get array of tabs call event listeners (aka main function)
+const logTabs = (tabs) => {
+    const popupList = document.querySelector('.tablist-items');
 
-    // get all tab URLs and push to array
+    // get all tab URLs and render list
     for (let tab of tabs) {
-      // tab.url requires the `tabs` permission
-      urlList.push(tab.url);
+        createTabList(tab, popupList);  
     }
 
-    // append URLs as li items to popup
-    urlList.forEach(function(url) {
-	const listItem = document.createElement('li');
-	listItem.appendChild(document.createTextNode(url));
-	popupList.appendChild(listItem);
-    });   
-}
+    // creat eventListeners
+    tabListEvents();
+};
 
-function onError(error) {
+// create list item and append it to popup
+const createTabList = (tab, popupList) => {
+    const listItemInput = document.createElement('input');
+    listItemInput.setAttribute('type', 'checkbox');
+    listItemInput.setAttribute('id', tab.id);
+    listItemInput.setAttribute('name', 'tab');
+    listItemInput.setAttribute('value', tab.url);
+
+    const listItemLabel = document.createElement('label');
+    listItemLabel.setAttribute('for', tab.id);
+    listItemLabel.appendChild(document.createTextNode(tab.title));
+
+    const listItem = document.createElement('li');
+    listItem.setAttribute('class', 'tablist-checkbox-wrapper');
+    listItem.appendChild(listItemInput);
+    listItem.appendChild(listItemLabel);
+
+    popupList.appendChild(listItem);
+};
+
+// create all needed event listeners
+const tabListEvents = () => {
+    // add event listener on wrapper and check for class?
+    const toggleSelect = document.getElementById('selectAll'),
+          deleteBtn = document.querySelector('.tablist-delete-btn'),
+          exportBtn = document.querySelector('.tablist-export-btn'),
+          listBtn = document.querySelector('.tablist-list-btn');
+
+    // check or uncheck all items in tablist
+    toggleSelect.addEventListener('click', (event) => {
+        // TODO: save all previosly checked?
+        const checkAllItems = event.target.checked;
+        const checkboxes = Array.from(document.getElementsByName('tab'));
+        checkboxes.map(checkbox => {
+            if(checkAllItems) {
+                checkbox.checked = true;
+            } else {
+                checkbox.checked = false;
+            }
+        });
+    });
+
+    // collect and delete all checked items
+    deleteBtn.addEventListener('click', () => {
+        console.log('click delete');
+        const checkboxes = Array.from(document.getElementsByName('tab'));
+        let selectedTabIds = [];
+        checkboxes.map(checkbox => {
+            if(checkbox.checked) {
+                console.log(checkbox);
+                selectedTabIds.push(parseInt(checkbox.id));
+            }
+        });
+        const removing = browser.tabs.remove(selectedTabIds);
+        removing.then(closePopup, onError);
+    });
+
+    exportBtn.addEventListener('click', () => {
+        console.log('click export');
+        const checkboxes = Array.from(document.getElementsByName('tab'));
+        let selectedTabUrls = [];
+        checkboxes.map(checkbox => {
+            if(checkbox.checked) {
+                selectedTabUrls.push(checkbox.value);
+            }
+        });
+        const creating = browser.tabs.create({
+            // data urls not possible: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/create
+            // add array to storage and retrieve it on new site:
+            // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage
+            // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/user_interface/Extension_pages
+            //url:'data:text/plain;charset=utf-8;base64,' + btoa(selectedTabUrls.join(''))
+        });
+        //creating.then(closePopup, onError);
+    });
+};
+
+const closePopup = () => {
+    console.log('done, closing');
+    window.close();  
+};
+
+const onError = (error) => {
   console.log(`Error: ${error}`);
-}
+};
 
 const querying = browser.tabs.query({});
 querying.then(logTabs, onError);
