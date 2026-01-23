1881458900f1189dfd60c0557cb22075953ccde8
Daniel Vogel <hello@dnlvgl.com>
2026-01-23 22:40:55 +0100

    Add web-ext config file to ignore files in the artifact

---
+++ web-ext-config.mjs
@@ -0,0 +1,17 @@
+export default {
+  build: {overwriteDest: true},
+  run: {
+    startUrl: ["about:debugging"], // Open URLs on start
+  },
+  ignoreFiles: [
+    'docs/**',
+    'screenshots/**',
+    'test/**',
+    '.git',
+    'node_modules',
+    'biome.json',
+    'package.json',
+    'package-lock.json',
+    'web-ext-config.cjs'
+  ]
+};
