3448fb3eb2b0f152921fbda3de274f408219c0fc
Daniel Vogel <hello@dnlvgl.com>
2026-02-11 22:49:28 +0100

    format export page and adjust css styling

--- tablist/tablist.js
+++ tablist/tablist.js
@@ -1,5 +1,21 @@
+const createPage = (storage) => {
+  updateTitle();
+  createTabList(storage);
+};
+
+const updateTitle = () => {
+  const now = new Date();
+  const date = now.toLocaleDateString('de-DE', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
+  const time = now.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' });
+  document.title = `Tablist Export ${date} ${time}`;
+};
+
 // get all links from storage an display in list
-const showList = (storage) => {
+const createTabList = (storage) => {
   const listElement = document.querySelector('.urlist');
   storage.selectedTabs.links.forEach((item) => {
     const listItem = document.createElement('li');
@@ -12,4 +28,4 @@ const onError = (error) => {
   console.log(`Error: ${error}`);
 };
 
-browser.storage.local.get('selectedTabs').then(showList, onError);
+browser.storage.local.get('selectedTabs').then(createPage, onError);
